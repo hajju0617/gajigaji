3719f528810d02216c877782bd2654fa6705ab3c
hobbang <songyerim1@naver.com>
2024-07-31 16:13:49 +0900

    0731

---
+++ src/main/java/com/green/gajigaji/budget/jpa/BudgetRepository.java
@@ -0,0 +1,7 @@
+package com.green.gajigaji.budget.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BudgetRepository extends JpaRepository<Budget, Long> {
+
+}
