66c2252ea47b9373e38acce7b036ce3c58124b49
hobbang <songyerim1@naver.com>
2024-07-31 16:13:49 +0900

    0731

---
+++ src/main/java/com/green/gajigaji/plan/jpa/Plan.java
@@ -0,0 +1,39 @@
+package com.green.gajigaji.plan.jpa;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity(name = "planMaster")
+@Setter
+@Getter
+public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long planSeq;
+
+    @ManyToOne
+    @JoinColumn(name = "plan_party_seq", nullable = false)
+    private Party party;
+
+    @Column(nullable = false)
+    private LocalDate planStartDt;
+
+    @Column(nullable = false)
+    private LocalDateTime planStartTime;
+
+    @Column(nullable = false)
+    private int planCompleted;
+
+    @Column(nullable = false)
+    private String planTitle;
+
+    @Column(nullable = false)
+    private String planContents;
+
+    @Column(nullable = false)
+    private String planLocation;
+}
