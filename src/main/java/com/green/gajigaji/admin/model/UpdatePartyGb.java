277177a0096264d0418ffbc4076321ccc6c75da3
hajju0617 <joowon023617@gmail.com>
2024-08-12 19:24:09 +0900

    Hajju - swagger 보완

--- src/main/java/com/green/gajigaji/admin/model/UpdatePartyGb.java
+++ src/main/java/com/green/gajigaji/admin/model/UpdatePartyGb.java
@@ -1,5 +1,6 @@
 package com.green.gajigaji.admin.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,9 +8,16 @@ import lombok.Setter;
 @Setter
 public class UpdatePartyGb {
 
+    @Schema(example = "1", description = "모임 PK")
     private long partySeq;
+
+    @Schema(example = "1", description = "모임 상태값")
     private int num;
+
+    @Schema(example = "abc123@naver.com", description = "유저 이메일")
     private String userEmail;
+
+    @Schema(example = "축하합니다 모임이 생성 되었습니다.", description = "메일에 넣을 내용")
     private String text;
 
 }
